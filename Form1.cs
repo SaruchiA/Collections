d0e2000492dc884a6ed9e74a9a2f06d0cb6e3f48
Saruchi Arora <arora.saruchi@gmail.com>
2015-01-16 16:26:40 -0800

    List of processes
    1. show sorted list of running processes
    2. show filtered list of processes filtered based on given text
    3. Splitting process name based on '(' and ')' to display only the name
    of process instead of System. Diagnostic.process.chrome

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary <String,int > d1 = new Dictionary <String,int> ();
+            d1.Add("saruchi",1);
+            for (int i = 0; i < d1.Count; i++)
+            {
+                d1.Add("abc"+ i,i);
+                
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var x = System.Diagnostics.Process.GetProcesses();
+            String userInputProc,processStr;
+            List <String> SingleProcessSplit=new List<String> ();
+            List<String> ConcatProcesses = new List<String>();
+            int idxPara,idxPara2;
+            List<String> listOfrunningProcess = new List<String>();
+            List<String> filteredList = new List<String>();
+            foreach (var item in x)
+            {                
+              //  Console.WriteLine(item.ProcessName + "is running with process id " + item.Id);
+                listOfrunningProcess.Add(item+"" );
+            }
+            Console.WriteLine("total no. of processes are:"+listOfrunningProcess.Count());
+            listOfrunningProcess.Sort();
+            Console.WriteLine("\r\n \r\n \r\n part 1: list of processes in ascending order \r\n");
+            for (int i = 0; i < listOfrunningProcess.Count; i++) 
+            {
+                processStr = listOfrunningProcess[i];
+                //idxPara =29;
+                //idxPara2 = list[i].LastIndexOf (')');
+                SingleProcessSplit.AddRange( processStr.Split(new string[] { "(", ")" }, StringSplitOptions.None));
+                for (int f = 1; f < SingleProcessSplit.Count; f+=2)
+                {
+                    ConcatProcesses.Add(SingleProcessSplit[f]);
+                }
+                SingleProcessSplit.Clear();
+                //finalStr = d.Substring(idxPara,idxPara2 );           
+                
+            }
+            for (int i = 0; i < ConcatProcesses.Count; i++)
+            {
+                Console.WriteLine(ConcatProcesses [i]);
+            }
+            //list.Reverse();
+            Console.WriteLine("\r\n \r\n \r\n part 1: list of processes in descending order \r\n");
+            for (int i = ConcatProcesses.Count - 1; i > 0; i--)
+            {
+                Console.WriteLine(ConcatProcesses[i]);
+            }
+
+            Console.WriteLine("enter the process name that needs to be added in list2 ");
+            userInputProc = Console.ReadLine();
+            Console.WriteLine("\r\n \r\n part 2:- show processes with particular suffix \r\n");
+           
+            for (int i = 0; i < listOfrunningProcess.Count; i++)
+            {
+                if (listOfrunningProcess[i] == userInputProc)
+                {
+                    filteredList.Add(listOfrunningProcess[i]+"");
+                }
+            }
+
+            for (int i = 0; i < filteredList.Count; i++)
+            {
+               Console.WriteLine(filteredList[i] );
+            } 
+            Console.ReadKey();
+           
+
+        }
+    }
+}
