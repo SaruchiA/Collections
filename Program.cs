777257dc500d4d46230addee962364b0d9efbe9f
Saruchi Arora <arora.saruchi@gmail.com>
2015-01-19 17:52:13 -0800

    Sorts and filters processes using List and Dictionary
    added methods for
    1. workWithLists()
    2. workWithDictionary()

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,68 +9,95 @@ namespace Collections
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            
-            var x = System.Diagnostics.Process.GetProcesses();
-            String userInputProc,processStr;
-            List <String> SingleProcessSplit=new List<String> ();
-            List<String> ConcatProcesses = new List<String>();
-            int idxPara,idxPara2;
-            List<String> listOfrunningProcess = new List<String>();
-            List<String> filteredList = new List<String>();
-            foreach (var item in x)
-            {                
-              //  Console.WriteLine(item.ProcessName + "is running with process id " + item.Id);
-                listOfrunningProcess.Add(item+"" );
-            }
-            Console.WriteLine("total no. of processes are:"+listOfrunningProcess.Count());
-            listOfrunningProcess.Sort();
-            Console.WriteLine("\r\n \r\n \r\n part 1: list of processes in ascending order \r\n");
-            for (int i = 0; i < listOfrunningProcess.Count; i++) 
-            {
-                processStr = listOfrunningProcess[i];
-                //idxPara =29;
-                //idxPara2 = list[i].LastIndexOf (')');
-                SingleProcessSplit.AddRange( processStr.Split(new string[] { "(", ")" }, StringSplitOptions.None));
-                for (int f = 1; f < SingleProcessSplit.Count; f+=2)
-                {
-                    ConcatProcesses.Add(SingleProcessSplit[f]);
-                }
-                SingleProcessSplit.Clear();
-                //finalStr = d.Substring(idxPara,idxPara2 );           
-                
-            }
-            for (int i = 0; i < ConcatProcesses.Count; i++)
-            {
-                Console.WriteLine(ConcatProcesses [i]);
-            }
-            //list.Reverse();
-            Console.WriteLine("\r\n \r\n \r\n part 1: list of processes in descending order \r\n");
-            for (int i = ConcatProcesses.Count - 1; i > 0; i--)
-            {
-                Console.WriteLine(ConcatProcesses[i]);
-            }
 
-            Console.WriteLine("enter the process name that needs to be added in list2 ");
-            userInputProc = Console.ReadLine();
-            Console.WriteLine("\r\n \r\n part 2:- show processes with particular suffix \r\n");
-           
-            for (int i = 0; i < listOfrunningProcess.Count; i++)
-            {
-                if (listOfrunningProcess[i] == userInputProc)
-                {
-                    filteredList.Add(listOfrunningProcess[i]+"");
-                }
-            }
-
-            for (int i = 0; i < filteredList.Count; i++)
-            {
-               Console.WriteLine(filteredList[i] );
-            } 
+            //workWithLists();          
+            workWithDictionary();
             Console.ReadKey();
-           
-
         }
+
+        private static void workWithLists()
+        {
+            var x = Process.GetProcesses();
+            var inputProcess = "";
+            List<Process> running_processes = new List<Process>();
+            List<String> Process_Name = new List<String>();
+            running_processes.AddRange(x);
+            foreach (var item in x)
+            {
+                Process_Name.Add("" + item.ProcessName);
+                Console.WriteLine(item.ProcessName + " is running with process id " + item.Id);
+            }
+            Process_Name.Sort();
+            Console.WriteLine("List of Processes sorted in Ascending order:- ");
+            foreach (var item in Process_Name)
+            {
+                Console.WriteLine("\n" + item);
+            }
+
+            Console.WriteLine("List of Processes sorted in Descending order:- ");
+            Process_Name.Reverse();
+            foreach (var item in Process_Name)
+            {
+                Console.WriteLine("\n" + item);
+            }
+
+            Console.WriteLine("Enter the process name:- ");
+            inputProcess = Console.ReadLine();
+
+            foreach (var item in running_processes)
+            {
+                if (inputProcess == item.ProcessName)
+                    Console.WriteLine("Process " + item.ProcessName + " is running with ID " + item.Id);
+            }
+        } // end of method workWithLists()
+
+        private static void workWithDictionary()
+        {
+            Dictionary <int,String> runningProcesses= new Dictionary<int,string> () ;
+            List<String> processName = new List<string>();
+            String inputProcess;
+            var x = Process.GetProcesses();
+            foreach (var item in x)
+            {
+                runningProcesses.Add(item.Id, item.ProcessName);
+            }
+            //runningProcesses.Values.ToList();
+            processName = runningProcesses.Values.ToList();
+            //foreach (var item in x)
+            //{
+            //    processName.Add(""+runningProcesses .Values );
+            //}
+            processName.Sort();
+
+            Console.WriteLine("\n\n Processes in ascending order:=");
+            foreach (var item in processName)
+            {
+                Console.WriteLine("\n" + item );
+            }
+
+            processName.Reverse();
+            Console.WriteLine("\n\n Processes in descending order:=");
+            foreach (var item in processName)
+            {
+                Console.WriteLine("\n" + item );
+            }
+
+            Console.WriteLine("Specify a process to be shown with its ID:");
+            inputProcess = Console.ReadLine();
+
+            foreach (var item in runningProcesses )
+            {
+                    if (inputProcess == item.Value )
+                    {
+                        Console.WriteLine(" Specified Process \" " + item.Value + "\" is running with id " + item.Key);
+                    }
+            }
+        
+        
+        } // end of method workWithDictionary() 
+
     }
 }
